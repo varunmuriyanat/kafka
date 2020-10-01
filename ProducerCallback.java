61d28b032ed31aed7f8553f1b67b1c41ff7bded8
varunmuriyanat <varunmuriyanat@gmail.com>
2020-10-01 18:56:07 -0400

    Added @Override back in

--- ProducerCallback.java
+++ ProducerCallback.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.RecordMetadata;
 
 public class ProducerCallback implements Callback {
+    @Override
     public void onCompletion(RecordMetadata recordMetadata, Exception e) {
         if (e != null) {
             e.printStackTrace();
